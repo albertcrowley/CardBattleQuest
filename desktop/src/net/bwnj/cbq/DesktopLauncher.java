d732994984e86dc90f0b3de644c62628f779299c
Al Crowley <al@bwnj.net>
2022-11-30 22:47:39 -0500

    First commit

---
+++ desktop/src/net/bwnj/cbq/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package net.bwnj.cbq;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import net.bwnj.cbq.CardBattleQuest;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("CardBattleQuest");
+		config.setWindowedMode(360, 640);
+		new Lwjgl3Application(new CardBattleQuest(), config);
+	}
+}
